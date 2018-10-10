611ea9207b998f672c9aa4886031e112259b5750
Edward Whelan <vroxing@gmail.com>
2018-10-10 14:49:53 -0400

    added some functions

--- scripts/index.js
+++ scripts/index.js
@@ -51,12 +51,15 @@ console.log(dayName)
 
 // python 102 and 103 in js;
 
+//Tip converter function
 const addItAllUp = () => {
     let mealTotal = prompt(`What is the total of your meal?`);
     let serviceLevel = prompt(`How was the service? ('good, fine, or bad')`);
     let split = prompt(`Number of people splitting the total?`);
     mealTotal = parseInt(mealTotal, 10);
     
+
+    //function to convert service into tip amount
     let tipTotal = (inputedServiceLevel) =>{
         if(inputedServiceLevel == `good`){
                 tipTotal = mealTotal * 0.2;
@@ -67,11 +70,37 @@ const addItAllUp = () => {
         }
     };
 
-    tipTotal(serviceLevel)
-    mealTipTotal = tipTotal + mealTotal;
-    splitTotal = mealTipTotal / split;
+    tipTotal(serviceLevel);
 
-    return console.log(`You tipped ${tipTotal}, your total with tip was ${mealTipTotal}. Split ${split} ways it would be ${splitTotal}`);
+    const mealTipTotal = tipTotal + mealTotal;
+    const splitTotal = mealTipTotal / split;
+
+    return console.log(`You tipped ${tipTotal}, your total with tip was ${mealTipTotal}. Split $${split} ways it would be $${splitTotal}`);
 };
 
-addItAllUp();
+// addItAllUp(); commented out 
+
+
+// let inputedValue = prompt(`What is your temperature in degrees celcius?`);
+
+const farenheitConverter = (numberToConvert) => {
+    inputedValue = parseFloat(numberToConvert);
+    return (numberToConvert * 1.8 + 32).toFixed(2);
+};
+
+// console.log(farenheitConverter(inputedValue));
+
+// const whatIsTheDay = prompt(`What is the day?(0-6)`)
+const doIGoToWork = (day) => {
+    if ((day < 1 ) || (day > 5)) {
+        return (`Sleep in. Hooraaaaay!`);
+    } else {
+        return (`Go to work fool.`);
+    }
+}
+// console.log(doIGoToWork(whatIsTheDay));
+
+
+for(let count = 0; count < 10; count++) {
+    console.log(count);
+}
